fac03a133842d82fef1582723e2e357581ddde1c
EC2 Default User <ec2-user@ip-172-31-3-243.ap-northeast-1.compute.internal>
2023-10-21 06:53:44 +0000

    [Final]課題完了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,6 +8,7 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
+      flash[:notice] = "Book was successfully created"
       redirect_to book_path(@book.id)
     else
       @books = Book.all
@@ -24,10 +25,14 @@ class BooksController < ApplicationController
   end
   
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    flash[:notice] = "Book was successfully updated"
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+     flash[:notice] = "Book was successfully updated"
+     redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :edit
+    end
   end
   
   def destroy
